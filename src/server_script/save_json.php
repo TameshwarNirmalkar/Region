46a1086eb723055ae174f159ed542ccdf92003a6
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2016-08-01 20:53:30 +0530

    issue fixed save resize

--- src/server_script/save_json.php
+++ src/server_script/save_json.php
@@ -7,7 +7,7 @@
 	$createfilename 	= $getfilename.$ext;
 	$sucessObject 		= '{"status": "200", "message":"Sucessfull Saved."}';
 	
-	$file 				= fopen("../stored_files/". $createfilename ,"w+") or die("can't open file");
+	$file 				= fopen("../stored_files/". $createfilename ,"w") or die("can't open file");
 	$encrypted 			= json_encode($objectarray);
 	fwrite($file, $encrypted);
 	fclose($file);
