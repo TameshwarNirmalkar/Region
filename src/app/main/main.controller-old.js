c57793a7edc80d099eda688fcb45cab2810e4cea
monikarathore <monika.singh.rathore@gmail.com>
2016-08-03 17:34:40 +0530

    images

--- src/app/main/main.controller-old.js
+++ src/app/main/main.controller-old.js
@@ -18,13 +18,15 @@
 		$scope.regionsimages = null;
 		$scope.isCanvasVisible = false;
 		$scope.activated = true;
-		$scope.filename = "page1";
-		$scope.imgpath = './assets/images/page1.jpg';
+		$scope.filename = "1-regions";
+		$scope.imgpath = './assets/images/1.jpg';
 		$scope.storedImageResources = null;
 		$scope.initialval = 0;
 		$scope.increment = 10;
 		// $scope.regionId = null;
 		/*Angular image element for get height and width in future reference*/
+		var angularEle = angular.element('.mid img');
+
 		CanvasService.getImages().$promise.then(function(res){
 			$scope.storedImageResources = res.imageoject;
 			$scope.regionsimages = $scope.storedImageResources.slice($scope.initialval, $scope.increment);
@@ -35,17 +37,12 @@
 		CanvasService.getRegionData($scope.filename).then(function(res){
 			if(res.data != undefined && res.data != ''){
 				CanvasService.loadJson(canvas, res.data);
+				$timeout(function(){
+					CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
+					angular.element('.canvas-container,canvas').css({width:angularEle.width()+'px', height:angularEle.height()+'px'});
+				},200);
 				$scope.activated = false;
 				$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
-				console.log('on load calling data');
-
-				$timeout(function(){
-					var wd = angular.element('.mid img').width();
-		   			var ht = angular.element('.mid img').height();
-					angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-					console.log(wd,':',ht);
-				},400);
-
 			}else{
 				$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
 				$scope.activated = false;
@@ -78,19 +75,8 @@
 			})
 		});
 
-		canvas.on("object:scaling", function(e){
-            var target = e.target;
-            var sX = target.scaleX;
-            var sY = target.scaleY;
-            target.width =  target.width*=sX;
-            target.height = target.height*=sY;
-            target.scaleX = 1;
-            target.scaleY = 1;
-        });
-
 		$scope.saveRegion = function(){
 			var formatjson = CanvasService.formateJson(canvas, $scope.region.options.target);
-			// console.log( formatjson );
 			CanvasService.saveRegion(formatjson, $scope.filename, canvas);
 		};
 
@@ -136,40 +122,36 @@
 				$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
 				$scope.activated = false;
 			});
+			$timeout(function(){
+				CanvasService.resizeCanvas(canvas, angular.element('.mid').width(), angular.element('.mid').height());
+			})
 		}
 
 		$scope.previousthumb = function(){
 			if($scope.initialval > 0){
+				canvas.clear();
 				$scope.initialval = $scope.initialval - 10;
 				$scope.increment = $scope.increment - 10;
 				$scope.regionsimages = $scope.storedImageResources.slice($scope.initialval, $scope.increment);
-				$scope.filename = $scope.initialval+'-regions';
-
-				var wd = angular.element('.mid img').width();
-		   		var ht = angular.element('.mid img').height();
+				$scope.filename = $scope.initialval+1+'-regions';
+				$scope.imgpath = $scope.regionsimages[0].img;
 				$timeout(function(){
-					angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-					////////console.log(wd,':',ht);
-				},300);
-				// $scope.imgpath = $scope.regionsimages[0].img;
-				// $timeout(function(){
-				// 	CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
-				// },200);
-				// CanvasService.getRegionData($scope.filename).then(function(res){
-				// 	if(res.data != undefined && res.data != ''){
-				// 		CanvasService.loadJson(canvas, res.data);
-				// 		$scope.activated = false;
-				// 		$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
-				// 	}else{
-				// 		$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
-				// 		$scope.activated = false;
-				// 	}					
-				// }, function (err) {
-				// 	$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
-				// 	$scope.activated = false;
-				// });
+					CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
+				},200);
+				CanvasService.getRegionData($scope.filename).then(function(res){
+					if(res.data != undefined && res.data != ''){
+						CanvasService.loadJson(canvas, res.data);
+						$scope.activated = false;
+						$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
+					}else{
+						$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
+						$scope.activated = false;
+					}					
+				}, function (err) {
+					$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
+					$scope.activated = false;
+				});
 			}else{
-				canvas.clear();
 				$scope.initialval = $scope.initialval;
 				$scope.increment = $scope.increment;
 				return false;
@@ -178,38 +160,30 @@
 
 		$scope.nextthumb = function(){
 			if($scope.increment < $scope.storedImageResources.length ){
+				canvas.clear();
 				$scope.initialval = $scope.increment;
 				$scope.increment = $scope.increment+10;
 				$scope.regionsimages = $scope.storedImageResources.slice($scope.initialval, $scope.increment);
-				$scope.filename = $scope.initialval+'-regions';
-
-				var wd = angular.element('.mid img').width();
-		   		var ht = angular.element('.mid img').height();
+				$scope.filename = $scope.initialval+1+'-regions';
+				$scope.imgpath = $scope.regionsimages[0].img;
 				$timeout(function(){
-					angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-					//////console.log(wd,':',ht);
-				},300);
-				// canvas.clear();
-				// $scope.imgpath = $scope.regionsimages[0].img;
-				// $timeout(function(){
-				// 	CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
-				// },200);
-				 // CanvasService.getRegionData($scope.filename).then(function(res){
-				// 	if(res.data !== undefined && res.data !== ''){
-				// 		CanvasService.loadJson(canvas, res.data);
-				// 		$scope.activated = false;
-				// 		$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
-				// 	}else{
-				// 		$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
-				// 		$scope.activated = false;
-				// 	}					
-				// }, function (err) {
-				// 	$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
-				// 	$scope.activated = false;
-				// });
+					CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
+				},200);
+				CanvasService.getRegionData($scope.filename).then(function(res){
+					if(res.data !== undefined && res.data !== ''){
+						CanvasService.loadJson(canvas, res.data);
+						$scope.activated = false;
+						$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
+					}else{
+						$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
+						$scope.activated = false;
+					}					
+				}, function (err) {
+					$mdToast.show( $mdToast.simple().theme("error-toast").textContent('Empty Canvas').position('top right').hideDelay(3000) );
+					$scope.activated = false;
+				});
 			}
 			else{
-					canvas.clear();
 					$scope.initialval = $scope.initialval;
 					$scope.increment = $scope.increment;
 					return false;
@@ -217,30 +191,20 @@
 		}
 
 		$scope.previousRegion = function(filename){
-			//alert('callig');
 			var getPath = parseInt(filename.match(/\d+/)[0]) - 1;
 			// var filterfile = _.some($scope.regionsimages, function (data) {
 			// 		return JSON.parse(data.imageid) === getPath;
 			// });
 			if(getPath > 0){
 				canvas.clear();
-				$scope.filename = 'page'+getPath;
-				$scope.imgpath = "./assets/images/page"+getPath+".jpg";
-				// $timeout(function(){
-				// 	// CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
-				// 	angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-				// });
+				$scope.filename = getPath+'-regions';
+				$scope.imgpath = "./assets/images/"+getPath+".jpg";
+				$timeout(function(){
+					CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
+				});
 				CanvasService.getRegionData($scope.filename).then(function(res){
 					if(res.data !== undefined && res.data !== ''){
 						CanvasService.loadJson(canvas, res.data);
-
-						var wd = angular.element('.mid img').width();
-				   		var ht = angular.element('.mid img').height();
-						$timeout(function(){
-							angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-							////console.log(wd,':',ht);
-						},300);
-
 						$scope.activated = false;
 						$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
 					}else{
@@ -265,22 +229,13 @@
 			if(getPath <= $scope.storedImageResources.length){
 				canvas.clear();
 				$scope.filename = getPath+'-regions';
-				$scope.imgpath = "./assets/images/page"+getPath+".jpg";
-				// $timeout(function(){
-				// 	// CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
-				// 	angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-				// },200);
+				$scope.imgpath = "./assets/images/"+getPath+".jpg";
+				$timeout(function(){
+					CanvasService.resizeCanvas(canvas, angularEle.width(), angularEle.height());
+				},200);
 				CanvasService.getRegionData($scope.filename).then(function(res){
 					if(res.data !== undefined && res.data !== ''){
 						CanvasService.loadJson(canvas, res.data);
-
-						var wd = angular.element('.mid img').width();
-				   		var ht = angular.element('.mid img').height();
-						$timeout(function(){
-							angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-							//console.log(wd,':',ht);
-						},300);
-
 						$scope.activated = false;
 						$mdToast.show( $mdToast.simple().theme("success-toast").textContent('Filled Canvas').position('top right').hideDelay(3000) );
 					}else{
@@ -297,32 +252,11 @@
 		}
 
 		angular.element($window).bind('resize', function(){
-			/*var angularEle = angular.element('.mid img');
-			angular.element('.canvas-container,canvas').css({width:angularEle.width()+'px', height:angularEle.height()+'px'});
-			console.log(angularEle.width(),':',angularEle.height());*/
-			
 			var wd = angular.element('.mid img').width();
-	   		var ht = angular.element('.mid img').height();
-			$timeout(function(){
-				angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-				//console.log(wd,':',ht);
-			},300);
+			var ht = angular.element('.mid img').height();
+			angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
 		});
-		angular.element(document).ready(function () {
-			var wd = angular.element('.mid img').width();
-	   		var ht = angular.element('.mid img').height();
-			$timeout(function(){
-				angular.element('.canvas-container,canvas').css({width:wd+'px', height:ht+'px'});
-				console.log(wd,':',ht);
-			},400);
-			
-		});
-		/*angular.element($window).bind('load', function(){
-			var angularEle = angular.element('.mid img');
-			angular.element('.canvas-container,canvas').css({width:angularEle.width()+'px', height:angularEle.height()+'px'});
-			console.log(angularEle.width(),':',angularEle.height());
-		});*/
+
 	}
 
 })();
-
